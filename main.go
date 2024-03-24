36822b63d84736a6390900a018bbaf1dae6a9aa7
Mani Movassagh <mani.mgh@gmail.com>
2024-03-24 10:16:57 +0100

    tt

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 func main() {
-
-
-	
+	var name []string
 	type Car struct {
 		name  string
 		model int
 	}
 
+
+
 	myCar := Car{
 		name:  "bmv",
 		model: 1244,
 	}
-fmt.Println(myCar.model)
+	fmt.Println(myCar.model,name)
 }
